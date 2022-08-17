df121011671600f636916bde7ffddec7149e79e5
Lerey <nailil2503@gmail.com>
2022-08-17 16:02:43 +0200

    Try to do an other way

--- app/src/main/java/fr/rey/navigationdrawer/navViewModel.kt
+++ app/src/main/java/fr/rey/navigationdrawer/navViewModel.kt
@@ -1,8 +1,7 @@
 package fr.rey.navigationdrawer
 
 import android.util.Log
-import androidx.compose.material.DrawerState
-import androidx.compose.material.DrawerValue
+import androidx.compose.material.*
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
